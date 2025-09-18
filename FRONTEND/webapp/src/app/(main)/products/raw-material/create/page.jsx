c1ed2eec5493f7366c6b037c8aa1a137bd0b529e
dooxsav <dooxsav@gmail.com>
2025-09-18 23:03:27 +0200

    travail sur le form card

--- FRONTEND/webapp/src/app/(main)/products/raw-material/create/page.jsx
+++ FRONTEND/webapp/src/app/(main)/products/raw-material/create/page.jsx
@@ -17,63 +17,16 @@ export default function CreateRawMaterial() {
 
   return (
     <div className={styles.container}>
-      <div className="card-global">
-        <h4 className="card-title">
+      <div className="form-card">
+        <h4 className="">
           <FolderPlus style={{ marginRight: "8px" }} />
           Enregistrement d&apos;une Matière Première
         </h4>
-
         <form onSubmit={handleSubmit(onSubmit)}>
-          <table className="table-light">
-            <tbody>
-              <tr>
-                <th style={{ minWidth: "150px" }}>
-                  <label htmlFor="name">Nom :</label>
-                </th>
-                <td>
-                  <input
-                    type="text"
-                    id="name"
-                    placeholder="Ex : Lait de brebis"
-                    {...register("name", {
-                      required: "Le nom est obligatoire.",
-                    })}
-                  />
-                  {errors.name && (
-                    <p className="form-error">{errors.name.message}</p>
-                  )}
-                </td>
-              </tr>
-
-              <tr>
-                <th style={{ minWidth: "150px" }}>
-                  <label htmlFor="name">Description :</label>
-                </th>
-                <td>
-                  <textarea
-                    type="text"
-                    id="name"
-                    placeholder="Description du produit"
-                    {...register("description")}
-                  />
-                  {errors.name && (
-                    <p className="form-error">{errors.name.message}</p>
-                  )}
-                </td>
-              </tr>
-
-              <tr>
-                <td
-                  colSpan={2}
-                  style={{ textAlign: "right", paddingTop: "10px" }}
-                >
-                  <button type="submit" className="btn-primary">
-                    Enregistrer
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className={styles.formcontrol}>
+            <label htmlFor="nom">Nom : </label>
+            <input type="text" name="nom" id="nom" />
+          </div>
         </form>
       </div>
     </div>
