430d453a3094ab1b64f73202652f7901d432c8b1
dooxsav <dooxsav@gmail.com>
2025-10-08 21:34:10 +0200

    article composition

---
+++ BACKEND/app/modules/article/schemas/article_composition/__init__.py
@@ -0,0 +1,3 @@
+# BACKEND\app\modules\article\schemas\article_composition\__init__.py
+
+from .articlecomposition_schema import ArticleCompositionSchema
---
+++ BACKEND/app/modules/article/schemas/article_composition/articlecomposition_schema.py
@@ -0,0 +1,10 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from app.modules.article.models import ArticleComposition
+from app.core.extensions import db
+
+class ArticleCompositionSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model: ArticleComposition
+        load_instance = True
+        include_fk = True
+        sqla_session = db.session
